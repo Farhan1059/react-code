f5fea05e98623a29de0151007c2f7e6a0f8f3d3c
farhan <farhanjaffer24@gmail.com>
2025-12-01 06:27:20 -0800

    UPDATE

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,40 @@ import React from "react"
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).
-render(
-  
-    <App />
 
+function MyApp() {
+    return (
+        <div>        
+            <h1>Custom App</h1>
+        </div>
+    )
+}
+
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'click me to visit google'      
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target="_blank">Vist Google</a>
 )
+
+const anotherUser = "chai aur code"
+
+const reactElement = React.createElement(
+    'a',
+    {href:'https://google.com', target: '_blank'},
+    'click me to visit google',
+    anotherUser
+)
+ReactDOM.createRoot(document.getElementById('root')).
+    render(
+
+        reactElement
+
+    )
